2f6b5e3e5ba6c186083b1e191a98975e3a0c7a4c
AngrySoundTech <AngrySoundTech@users.noreply.github.com>
2021-07-24 18:58:42 +0000

    deploy: d18fec751c29607a7ab37a9dbfda4f2739c59827

---
+++ _nuxt/static/1627153120/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627153120",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fde72f44"}}}}(null,"\u002F"));
